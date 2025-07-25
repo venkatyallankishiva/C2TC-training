60a764220e86b78c750b04285e7ad207f7b6e7ff
Yallanki Venkat Shiva Goud <yallankivenkatshiva2009@gmail.com>
2025-07-25 19:52:05 +0530

    Add files via upload

---
+++ ThreadMain.java
@@ -0,0 +1,12 @@
+
+public class ThreadMain {
+	public static void main(String[] args) {
+      ThreadDemo t1=new ThreadDemo();
+     
+     // ThreadDemo t2=new ThreadDemo();
+      t1.start();
+		//t2.start();	
+	}
+}
+
+
